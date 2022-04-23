c9552706c181ca80025a5acbb83eb46c362798c2
Adam Andrews <andrewsadam07@gmail.com>
2022-04-23 16:18:27 +0100

    finished header

--- components/Header.js
+++ components/Header.js
@@ -6,15 +6,13 @@ import {
 	HeartIcon,
 	PaperAirplaneIcon,
 	MenuIcon,
-    UserGroupIcon,
+	UserGroupIcon,
 } from '@heroicons/react/outline';
 
-import {
-	HomeIcon
-} from '@heroicons/react/solid';
+import { HomeIcon } from '@heroicons/react/solid';
 function Header() {
 	return (
-		<div>
+		<div className='shadow-sm border-b bg-white sticky top-0 z-50'>
 			<div className="flex justify-between max-w-6xl  mx-5 xl:mx-auto ">
 				<div className="relative  hidden lg:inline-grid w-24">
 					{/* Left */}
@@ -32,7 +30,7 @@ function Header() {
 					/>
 				</div>
 				{/* Middle */}
-				<div className='max-w-xs'>
+				<div className="max-w-xs">
 					<div className="relative mt-1 p-3 rounded-md">
 						<div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none ">
 							<SearchIcon className="h-5 w-5  text-gray-500" />
@@ -44,20 +42,27 @@ function Header() {
 						></input>
 					</div>
 				</div>
-                <div className='flex items-center justify-end space-x-4'>
-                <HomeIcon className='navBtn'/> 
-                <MenuIcon className='h-6 w-6 md:hidden cursor-pointer'/> 
-                <PaperAirplaneIcon className='navBtn'/>
-                <PlusCircleIcon className='navBtn'/>
-                <UserGroupIcon className='navBtn'/>
-                <HeartIcon className='navBtn'/>
-                <img className="h-10 w-10 rounded-full object-cover"src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/" alt ="hello"></img>
-                </div>
-                
+				<div className="flex items-center justify-end space-x-4">
+					<HomeIcon className="navBtn" />
+					<MenuIcon className="h-6 w-6 md:hidden cursor-pointer" />
+					<div className="relative navBtn">
+						<PaperAirplaneIcon className="navBtn rotate-45" />
+						<div className="absolute -top-1 -right-2 text-sx w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+							3
+						</div>
+					</div>
+					<PlusCircleIcon className="navBtn" />
+					<UserGroupIcon className="navBtn" />
+					<HeartIcon className="navBtn" />
+					<img
+						className="h-10 w-10 rounded-full object-cover"
+						src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/"
+						alt="hello"
+					></img>
+				</div>
 			</div>
 
 			{/* Right */}
-            
 		</div>
 	);
 }
