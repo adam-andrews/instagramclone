ebc8c29bbebcc233249ba886f76265432a38411c
Adam Andrews <andrewsadam07@gmail.com>
2022-04-24 16:50:24 +0100

    Finished Stories

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import faker  from 'faker';
+import { useEffect,useState } from 'react';
+import Story from './Story';
 function Stories() {
-  return (
-    <div>Stories</div>
-  )
+    const [suggestions,setSuggestions] = useState([])
+	useEffect(() => {
+		const suggestions = [...Array(20)].map((_, i) => ({
+			...faker.helpers.contextualCard(),
+			id: i,
+		}));
+        setSuggestions(suggestions)
+	}, []);
+
+	return <div>
+        {suggestions.map(profile => ( 
+            <Story key={profile.id} img={profile.avatar} username={profile.username}/>
+        ))}
+    </div>;
 }
 
-export default Stories
+export default Stories;
