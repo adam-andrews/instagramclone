1ad9a0d8334bf6e152a7d62845c809c2497d63e8
Adam Andrews <andrewsadam07@gmail.com>
2022-04-27 00:48:40 +0100

    Added story

--- components/Stories.js
+++ components/Stories.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import faker  from 'faker';
-import { useEffect,useState } from 'react';
+import faker from 'faker';
+import { useEffect, useState } from 'react';
 import Story from './Story';
-import {generateAvatar} from "./utils";
+import { generateAvatar } from './utils';
 function Stories() {
-    const [suggestions,setSuggestions] = useState([])
+	const [suggestions, setSuggestions] = useState([]);
 	useEffect(() => {
 		const suggestions = [...Array(20)].map((_, i) => ({
 			...faker.helpers.contextualCard(),
 			id: i,
-            img: generateAvatar(Math.random()),
+			img: generateAvatar(Math.random()),
 		}));
 
-        setSuggestions(suggestions)
+		setSuggestions(suggestions);
 	}, []);
 
-	return <div>
-        {suggestions.map(profile => ( 
-            <Story key={profile.id} img={profile.img} username={profile.username}/>
-        ))}
-    </div>;
+	return (
+		<div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll'>
+			{suggestions.map((profile) => (
+				<Story key={profile.id} img={profile.img} username={profile.username} />
+			))}
+		</div>
+	);
 }
 
 export default Stories;
