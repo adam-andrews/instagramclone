44e0992eea1f6976f6f7ac2ee95fe9d25433911c
Adam Andrews <andrewsadam07@gmail.com>
2022-04-27 01:00:12 +0100

    Finished stories

--- components/Stories.js
+++ components/Stories.js
@@ -16,7 +16,7 @@ function Stories() {
 	}, []);
 
 	return (
-		<div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll'>
+		<div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thumb-black'>
 			{suggestions.map((profile) => (
 				<Story key={profile.id} img={profile.img} username={profile.username} />
 			))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+    require('tailwind-scrollbar-hide')
+
 
   ],
 }
