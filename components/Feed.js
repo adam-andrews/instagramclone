bd37e1a4445093bbe13d19530c608ed0a432eac4
Adam Andrews <andrewsadam07@gmail.com>
2022-04-28 12:04:52 +0100

    Completed suggestions

--- components/Feed.js
+++ components/Feed.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Posts from './Posts';
 import Stories from './Stories';
 import MiniProfile from './MiniProfile';
+import Suggestions from './Suggestions';
+
 function Feed() {
 	return (
 		<main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
@@ -9,9 +11,12 @@ function Feed() {
 				<Stories />
                 <Posts/>
 			</section>
-			<section>
+			<section className='hidden xl:inline-grid md:col-span-1'>
+                <div className='fixed top-20'>
                 <MiniProfile/>
-				{/* Suggestions */}
+                <Suggestions/>
+                </div>
+                
 			</section>
 		</main>
 	);
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 function MiniProfile() {
-  return (
-    <div>MiniProfile</div>
-  )
+	return (
+		<div className='flex items-center justify-between mt-14 ml-10'>
+			<img
+				className="w-16 h-16 rounded-full border p-[2px]"
+				src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/"
+				alt=""
+			/>
+			<div className='flex-1 mx-4'>
+				<h2 className='font-bold'>Adam</h2>
+                <h3 className='flex items-center text-sm text-gray-500'>Welcome to instagram</h3>
+			</div>
+            <button className='text-blue-400 text-sm font-bold'>Sign Out</button>
+		</div>
+	);
 }
 
-export default MiniProfile
+export default MiniProfile;
