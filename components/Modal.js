1bf97c60c50ff6f966e761db6954a3c0957fc2b9
Adam Andrews <andrewsadam07@gmail.com>
2022-05-03 12:56:55 +0100

    Customized modal

--- components/Modal.js
+++ components/Modal.js
@@ -12,41 +12,77 @@ function Modal() {
 				className="fixed z-10 inset-0 overflow-y-auto"
 				onClose={setOpen}
 			>
+				<div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<Dialog.Overlay className="fixed inset-0 big-gray-500 bg-opacity-75 transition-opacity" />
+					</Transition.Child>
 
-			<div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-				<Transition.Child
-					as={Fragment}
-					enter="ease-out duration-300"
-					enterFrom="opacity-0"
-					enterTo="opacity-100"
-					leave="ease-in duration-200"
-					leaveFrom="opacity-100"
-					leaveTo="opacity-0"
-				>
-					<Dialog.Overlay className="fixed inset-0 big-gray-500 bg-opacity-75 transition-opacity" />
-				</Transition.Child>
+					<span
+						className="hidden sm:inline-block sm:align-middle sm:h-screen"
+						aria-hidden="true"
+					>
+						&#8203;
+					</span>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						enterTo="opacity-100 translate-y-0 sm:scale-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+						leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+					>
+						<div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+							<div>
+								<div>
+									<div className="mt-3 text-center sm:mt-5">
+										<Dialog.Title
+											as="h3"
+											className="text-lg leading-6 font-medium text-gray-900"
+										>
+											Upload a Photo
+										</Dialog.Title>
 
-				<span
-					className="hidden sm:inline-block sm:align-middle sm:h-screen"
-					aria-hidden="true"
-				>
-                    &#8203;
-                </span>
-                <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                > 
-                    <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'>
-                        Hello
-                    </div>
-                </Transition.Child>
-			</div>
-            </Dialog>
+										<div>
+											<input //ref={filePickerRef}
+												type="file"
+												id="file"
+												hidden
+												//onChange={addImage}
+											/>
+										</div>
+
+										<div className="mt-2">
+											<input
+												className="border-none focus:ring-0 w-full text-center"
+												type="text"
+												//ref={captionRef}
+                                                placeholder='Enter a caption...'
+											></input>
+										</div>
+									</div>
+								</div>
+								<div className="mt-5 sm:mt-6">
+									<button
+										type="button"
+										className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:border-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+									>
+										upload post
+									</button>
+								</div>
+							</div>
+						</div>
+					</Transition.Child>
+				</div>
+			</Dialog>
 		</Transition.Root>
 	);
 }
