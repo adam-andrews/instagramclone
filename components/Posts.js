6e85e8b11da71cb782a91957daeb02fa9489b896
Adam Andrews <andrewsadam07@gmail.com>
2022-05-03 15:03:35 +0100

    Fixed invalid hooks bug

--- components/Posts.js
+++ components/Posts.js
@@ -1,46 +1,32 @@
+import { onSnapshot } from 'firebase/firestore';
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Post from './Post';
+import {db} from "../firebase";
+import {collection,query,orderBy} from '@firebase/firestore';
 
-const posts = [
-	{
-		id: '123',
-		username: 'adam',
-		userImg:
-			'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/',
-		img: 'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/',
-		caption: 'This is a photo of my dog',
-	},
-	{
-		id: '123',
-		username: 'adam',
-		userImg:
-			'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/',
-		img: 'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/',
-		caption: 'This is a photo of my dog',
-	},
-	{
-		id: '123',
-		username: 'adam',
-		userImg:
-			'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/',
-		img: 'https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?cs=srgb&dl=pexels-helena-lopes-2253275.jpg&fm=jpg/',
-		caption: 'This is a photo of my dog',
-	},
-];
 function Posts() {
-	return (
-		<div>
-			{posts.map((post) => (
-				<Post
-					key={post.id}
-					userImg={post.userImg}
-					img={post.img}
-					username={post.username}
+	const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        return onSnapshot(query(collection(db, 'posts'), orderBy("timestamp","desc")), snapshot => {
+            setPosts(snapshot.docs);
+        });
+        
+    },[db]);
+
+    return (
+        <div>
+            {/* {posts.map((post) => (
+                <Post
+                    key={post.id}
+                    userImg={post.userImg}
+                    img={post.img}
+                    username={post.username}
                     caption={post.caption}
-				/>
-			))}
-		</div>
-	);
+                />
+            ))} */}
+        </div>
+    );
 }
 
 export default Posts;
